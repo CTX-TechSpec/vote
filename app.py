76dd912db2f96c7d106ab03fbd6ba99c433d0f6f
Your Name <mayank@emial.com>
2017-09-15 08:20:56 -0700

    reset to pet choices

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "No..")
-option_b = os.getenv('OPTION_B', "Yes!!")
+option_a = os.getenv('OPTION_A', "Dog")
+option_b = os.getenv('OPTION_B', "Cat")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
